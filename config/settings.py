2ccb9031020ebd472865ab8ae2cd625a41c16d2b
nack1400 <nack1400@naver.com>
2023-10-06 22:45:38 -0700

    Add medias model

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,8 @@ CUSTOM_APPS = [
     "reviews.apps.ReviewsConfig",
     "wishlists.apps.WishlistsConfig",
     "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
+    "direct_messages.apps.DirectMessagesConfig",
 ]
 
 
