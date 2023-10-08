adb8e67a794826bbe7427f602a68b10acc5c75fb
nack1400 <nack1400@naver.com>
2023-10-08 03:31:17 -0700

    Add template

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
-def say_hello(request):
-    return HttpResponse("Hello!!")
+def see_all_rooms(request):
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_room.html",
+        {
+            "rooms": rooms,
+            "title": "Hello!",
+        },
+    )
+
+
+def see_one_room(request, room_pk):
+    try:
+        room = Room.objects.get(pk=room_pk)
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "room": room,
+            },
+        )
+    except Room.DoesNotExist:
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "not_found": True,
+            },
+        )
