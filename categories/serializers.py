bde8217fbab46d9b83d43c212b8904b5a80241c7
nack1400 <nack1400@naver.com>
2023-10-08 04:40:24 -0700

    Add post api, is_valid

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,8 +2,13 @@ from rest_framework import serializers
 
 
 class CategorySerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.CharField(
+        max_length=15,
+    )
+    created_at = serializers.DateTimeField(read_only=True)
 
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
-    created_at = serializers.DateTimeField()
