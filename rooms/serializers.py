8c7e9e85da90664a4a32c6e2ba3f1650b72bbef0
nack1400 <nack1400@naver.com>
2023-10-16 09:00:40 -0700

    Add rooms api

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,10 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
+from reviews.serializers import ReviewSerializer
 from categories.serializers import CategorySerializer
 
 
-class AmenitySerializer(ModelSerializer):
+class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Amenity
         fields = (
@@ -13,17 +14,38 @@ class AmenitySerializer(ModelSerializer):
         )
 
 
-class RoomDetailSerializer(ModelSerializer):
+class RoomDetailSerializer(serializers.ModelSerializer):
+    owner = TinyUserSerializer(read_only=True)
+    amenities = AmenitySerializer(
+        read_only=True,
+        many=True,
+    )
+    category = CategorySerializer(
+        read_only=True,
+    )
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+    reviews = ReviewSerializer(
+        many=True,
+        read_only=True,
+    )
 
-    owner = TinyUserSerializer()
-    amenities = AmenitySerializer(many=True)
-    category = CategorySerializer()
     class Meta:
-        model = Amenity
+        model = Room
         fields = "__all__"
 
+    def get_rating(self, room):
+        return room.rating()
 
-class RoomListSerializer(ModelSerializer):
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
+
+
+class RoomListSerializer(serializers.ModelSerializer):
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+
     class Meta:
         model = Room
         fields = (
@@ -32,4 +54,13 @@ class RoomListSerializer(ModelSerializer):
             "country",
             "city",
             "price",
+            "rating",
+            "is_owner",
         )
+
+    def get_rating(self, room):
+        return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
